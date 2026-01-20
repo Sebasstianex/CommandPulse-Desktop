20a01d679385b100afc4a4c92d362f9792a8ed1f
Sebasstianex <gtasebastian777@gmail.com>
2026-01-20 18:36:42 +0100

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,50 @@
+const { app, BrowserWindow, shell, Menu, Tray } = require('electron');
+const path = require('path');
+
+let mainWindow;
+let tray;
+
+const APP_URL = 'https://botcommandpulse.com';
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 768,
+    icon: path.join(__dirname, '../assets/icon.png'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    },
+    show: false,
+    titleBarStyle: 'default',
+    autoHideMenuBar: false
+  });
+
+  mainWindow.loadURL(APP_URL);
+  
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
